090e1d8843c19f48c7f99ea4cd1b62e9301e9162
rja907 <kmrrajj@gmail.com>
2018-02-02 15:57:21 +0530

    added CommentBox functionality

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -14,11 +14,11 @@ describe('App', () => {
   // });
 
   //Actual test to be conducted for the application.
-  it('shows a comment box', ()=>{
-    let component;
-    beforeEach(()=>{
-      component = renderComponent(App);
-    })
+  let component;
+  beforeEach(()=>{
+    component = renderComponent(App);
+  });
+  it('shows a comment box', () => {
     expect(component.find('.comment-box')).to.exist;
   });
 });
