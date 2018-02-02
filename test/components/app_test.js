56777692006b23799f7755b4db8470edb947bb0b
rja907 <kmrrajj@gmail.com>
2018-02-02 15:09:36 +0530

    added CommentBox

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,24 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+//Use 'describe' to group together similar tests.
+describe('App', () => {
+  // ****Starting and learning test format****
+  // //Use it to test a single attr of a target.
+  // it('shows the correct text', () => {
+  //   //Create an instance of App.
+  //   const component = renderComponent(App);
+  //   //Use 'expect' to make an assertion about a target.
+  //   //expect(<thing we want to make an assertion about>).to.have.class('<value we expect>');
+  //   expect(component).to.contain('React simple starter');
+  // });
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+  //Actual test to be conducted for the application.
+  it('shows a comment box', ()=>{
+    let component;
+    beforeEach(()=>{
+      component = renderComponent(App);
+    })
+    expect(component.find('.comment-box')).to.exist;
   });
 });
